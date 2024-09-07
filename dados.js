2c4f7822f59fcf36ffef2cc994444883802fed16
Aline Rodrigues <rdrgs.alinemkt@gmail.com>
2024-09-07 17:11:49 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -2,21 +2,25 @@ let dados = [
     {
         titulo: "Rayssa Leal",
         descricao: "Rayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma.Ela se tornou a mais jovem medalhista olímpica da história do Brasil, conquistando a prata nos Jogos de Tóquio.Sua história de superação e paixão pelo skate a tornou um ícone para a nova geração.",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
     },
     {
         titulo: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros com suas performances excepcionais. Bicampeã olímpica e a maior medalhista da história do Brasil nos Jogos Olímpicos, Rebeca se destacou por sua força, elegância e determinação. Suas conquistas históricas, como o ouro no salto e a prata no individual geral nos Jogos de Tóquio, inspiraram uma nova geração de atletas e consolidaram seu lugar entre as maiores ginastas do mundo. Com sua trajetória de superação e talento, Rebeca Andrade se tornou um verdadeiro símbolo do esporte brasileiro.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "gisnatica ginasta ginástica ouro"
     },
     {
         titulo: "Beatriz Souza",
         descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos. Com sua técnica apurada e determinação, ela conquistou a medalha de no judô, inspirando jovens atletas e mostrando a força do esporte feminino no Brasil.",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judok judo judoca"
     },
     {
         titulo: "Tatiana Weston-Webb",
         descricao: "Tatiana Weston-Webb é uma surfista profissional brasileira-americana que conquistou o mundo com sua habilidade e determinação nas ondas. Nascida em Porto Alegre, mas radicada no Havaí, ela já acumula diversos títulos e conquistas importantes em sua carreira, como a medalha de prata nos Jogos Olímpicos de Tóquio. Sua paixão pelo surfe, aliada a um talento nato, a transformou em um dos maiores nomes do esporte, inspirando uma nova geração de surfistas. Com sua presença marcante nos principais campeonatos do mundo, Tatiana Weston-Webb continua a fazer história e a levar o nome do Brasil para o alto.",
-        link: "https://pt.wikipedia.org/wiki/Tatiana_Weston-Webb"
+        link: "https://pt.wikipedia.org/wiki/Tatiana_Weston-Webb",
+        tags: "surfista surf surfbr"
     }
 ];
